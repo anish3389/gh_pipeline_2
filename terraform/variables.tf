dd34081ed86e6073be001dda27d1d9ac676a6a96
Anish Sapkota <sapkotaanish000@gmail.com>
2024-06-03 17:09:32 +0545

    updated variables with types and default values

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -32,15 +32,74 @@ variable "silo" {
 }
 
 
-variable "ami_id" {}
-variable "instance_type" {}
-variable "sg_id" {}
-variable "subnet_id" {}
-variable "monitoring" {}
-variable "associate_public_ip_address" {}
-variable "create_iam_instance_profile" {}
-variable "ssm_policy" {}
-variable "s3_policy" {}
-variable "instance_name" {}
-variable "user_data_file" {}
-variable "bucket_name" {}
+variable "ami_id" {
+  description = "AMI of the server (Default is Ubuntu 22.04)"
+  type        = string
+  default     = "ami-012485deee5681dc0"
+}
+
+variable "instance_type" {
+  description = "Instance Type"
+  type        = string
+  default     = "t2.nano"
+}
+
+variable "sg_id" {
+  description = "Security Group ID for the instance"
+  type        = string
+  default     = ""
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where instance will be placed"
+  type        = string
+  default     = ""
+}
+
+variable "monitoring" {
+  description = "Whether to enable monitoring for the server"
+  type        = bool
+  default     = false
+}
+
+variable "associate_public_ip_address" {
+  description = "Whether to associate public ip to the server"
+  type        = bool
+  default     = false
+}
+
+variable "create_iam_instance_profile" {
+  description = "Whether to create IAM Instance profile and attach to the server"
+  type        = bool
+  default     = false
+}
+
+variable "ssm_policy" {
+  description = "Whether to attach SSM policy to the server"
+  type        = bool
+  default     = false
+}
+
+variable "s3_policy" {
+  description = "S3 Policy to attach to the server"
+  type        = string
+  default     = ""
+}
+
+variable "instance_name" {
+  description = "Name of the instance"
+  type        = string
+  default     = ""
+}
+
+variable "user_data_file" {
+  description = "File where user data is placed"
+  type        = string
+  default     = ""
+}
+
+variable "bucket_name" {
+  description = "Name of the bucket to create"
+  type        = string
+  default     = ""
+}
